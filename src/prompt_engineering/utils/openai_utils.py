865c067b0d8894c1b01c4efc6b1bf9223851cbd2
YeeJunOw19 <yeejunow@gmail.com>
2025-02-15 13:13:47 -0600

    Prompt Engineering: Utilities
    Added utilities to be used universally in the entire prompt engineering workflow

---
+++ src/prompt_engineering/utils/openai_utils.py
@@ -0,0 +1,82 @@
+
+import asyncio
+import re
+import backoff
+import openai
+import polars as pl
+from datetime import datetime
+
+
+def answer_style_parser(questions: list[str]) -> list[str]:
+    # For each of the questions, add how OpenAI should style its answers
+    parsed_questions = []
+    for question in questions:
+        temp_str = f'{question} -- Please output only the final numerical number at the end with the prefix ####. For example: ####[ANSWER]'
+        parsed_questions.append(temp_str)
+
+    return parsed_questions
+
+
+def answer_extractor(answers: list[str]) -> list[int | float]:
+    # Create a new list that only contains the correct answer output
+    numerical_answers = []
+    for answer in answers:
+        try:
+            num_ans = answer.split("####")[1]
+        except IndexError:
+            num_ans = answer
+
+        num_ans = num_ans.strip()
+        numerical_answers.append(num_ans)
+
+    # Replace all non-numerical character with ""
+    cleaned_answers = []
+    for answer in numerical_answers:
+        x = re.sub(r"[a-zA-Z$%, ]", "", answer).strip()
+        if "." in x:
+            number = float(x)
+        else:
+            number = int(x)
+
+        cleaned_answers.append(number)
+
+    return cleaned_answers
+
+
+@backoff.on_exception(backoff.expo, openai.RateLimitError)
+async def chat_completion(openai_client, prompt: str, model: str, temperature=float) -> str:
+    # Create the message and send it to OpenAI
+    messages = [{"role": "user", "content": prompt}]
+    response = await openai_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+    )
+
+    return response.choices[0].message.content
+
+
+def scoring_function(
+    ids: list[int], openai_answers: list[float], scoring_df: pl.LazyFrame, matching_cols: list[str],
+    model_name: str, model_temperature: float, run_method: str
+) -> pl.DataFrame:
+    # Create a base dataframe from the results of OpenAI
+    df = pl.DataFrame({"RowId": ids, "OpenAIAnswer": openai_answers})
+
+    # Join the scoring dataframe and perform scoring and metadata coding
+    df = (
+        df
+        .join(scoring_df.collect(), left_on=matching_cols[0], right_on=matching_cols[1], how="inner")
+        .with_columns(
+            pl.col("OpenAIAnswer").cast(pl.Float64).alias("OpenAIAnswer"),
+            pl.lit(model_name).alias("ModelName"),
+            pl.lit(model_temperature).alias("ModelTemperature"),
+            pl.lit(run_method).alias("RunStyle"),
+        )
+        .with_columns(
+            pl.when((pl.col("OpenAIAnswer") - pl.col("ExtractedAnswer")) == 0).then(1).otherwise(0).alias("IndCorrect"),
+            pl.lit(datetime.now().date()).alias("RunDate")
+        )
+    )
+
+    return df
---
+++ src/prompt_engineering/utils/question_generator.py
@@ -0,0 +1,58 @@
+
+import polars as pl
+import random
+from sqlalchemy.engine import Engine
+from src.data_ingestion.mdutils import motherduck_setup
+
+
+class QuestionGenerator:
+
+    def __init__(self, duck_engine: Engine, database_schema: str, table_name: str, n: int = 300, seed: int = 3825):
+        self.duck_engine = duck_engine
+        self.database_schema = database_schema
+        self.table_name = table_name
+        self.n = n
+        self.seed = seed
+        self.random_ids = self._random_id_generator()
+
+    def _random_id_generator(self) -> list[int]:
+        # From MotherDuck get a list of available IDs
+        query_string = f'SELECT Id FROM "{self.database_schema}".{self.table_name}'
+        ids = (
+            motherduck_setup.md_read_table(
+                duck_engine=self.duck_engine, md_schema=self.database_schema, md_table=self.table_name,
+                keep_columns=None, custom_query=query_string
+            )
+            .collect()
+            .to_series()
+            .to_list()
+        )
+
+        # From the list, randomly pick the number of questions that the user has specified
+        random.seed(self.seed)
+        random_ids = random.sample(ids, self.n)
+
+        return random_ids
+
+    def qa_generator(self, answer_schema: str, answer_table: str) -> dict:
+        # From MotherDuck, get the questions and answers and save them into a dictionary
+        question_string = f'SELECT Id, QuestionAsked FROM "{self.database_schema}".{self.table_name}'
+        answer_string = f'SELECT Id, ExtractedAnswer FROM "{answer_schema}".{answer_table}'
+
+        query_df = (
+            motherduck_setup.md_read_table(
+                duck_engine=self.duck_engine, md_schema=self.database_schema, md_table=self.table_name,
+                keep_columns=None, custom_query=question_string
+            )
+            .filter(pl.col("Id").is_in(self.random_ids))
+        )
+
+        answer_df = (
+            motherduck_setup.md_read_table(
+                duck_engine=self.duck_engine, md_schema=answer_schema, md_table=answer_table,
+                keep_columns=None, custom_query=answer_string
+            )
+            .filter(pl.col("Id").is_in(self.random_ids))
+        )
+
+        return {"questions": query_df, "answers": answer_df}
