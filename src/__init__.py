7ca63503b0939f20dc03442e054db61e47a458eb
YeeJunOw19 <yeejunow@gmail.com>
2025-02-16 12:50:35 -0600

    Updated Dagster Dependencies
    Updated Dagster dependencies and assets to include workflows from OPRO

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
 
 from dagster import Definitions
 from src.data_ingestion import raw_data_ingestion, vector_embedding_wf
-from src.prompt_engineering import tables_preprocessing, prompting_wf, data_modeling_wf
+from src.prompt_engineering import tables_preprocessing, prompting_wf, data_modeling_wf, opro_wf
 from src.jobs import run_raw_data_save, run_vector_embedding, run_prompt_engineering
 
 all_assets = [
     *raw_data_ingestion, *vector_embedding_wf, *tables_preprocessing, *prompting_wf,
-    *data_modeling_wf
+    *data_modeling_wf, *opro_wf
 ]
 
 defs = Definitions(
--- src/prompt_engineering/__init__.py
+++ src/prompt_engineering/__init__.py
@@ -1,6 +1,6 @@
 
 from dagster import load_assets_from_package_module
-from src.prompt_engineering import database_preprocessing, main_workflows, data_modeling
+from src.prompt_engineering import database_preprocessing, main_workflows, data_modeling, opro_implementation
 
 # Group name for run pipelines
 PROMPT_ENGINEERING = "prompt_engineering"
@@ -9,3 +9,4 @@ PROMPT_ENGINEERING = "prompt_engineering"
 tables_preprocessing = load_assets_from_package_module(package_module=database_preprocessing, group_name=PROMPT_ENGINEERING)
 prompting_wf = load_assets_from_package_module(package_module=main_workflows, group_name=PROMPT_ENGINEERING)
 data_modeling_wf = load_assets_from_package_module(package_module=data_modeling, group_name=PROMPT_ENGINEERING)
+opro_wf = load_assets_from_package_module(package_module=opro_implementation, group_name=PROMPT_ENGINEERING)
