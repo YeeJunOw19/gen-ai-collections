929feaf33ec8b06b5527b04bca7c917169104d23
YeeJunOw19 <yeejunow@gmail.com>
2025-02-15 18:06:05 -0600

    Adjusted Dagster High Level Configurations
    Adjusted Dagster configurations with the refactors and newly added workflows

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,13 @@
 
 from dagster import Definitions
 from src.data_ingestion import raw_data_ingestion, vector_embedding_wf
-from src.prompt_engineering import tables_preprocessing, basic_prompting_wf
+from src.prompt_engineering import tables_preprocessing, prompting_wf, data_modeling_wf
 from src.jobs import run_raw_data_save, run_vector_embedding, run_prompt_engineering
 
-all_assets = [*raw_data_ingestion, *vector_embedding_wf, *tables_preprocessing, *basic_prompting_wf]
+all_assets = [
+    *raw_data_ingestion, *vector_embedding_wf, *tables_preprocessing, *prompting_wf,
+    *data_modeling_wf
+]
 
 defs = Definitions(
     assets=all_assets,
--- src/prompt_engineering/__init__.py
+++ src/prompt_engineering/__init__.py
@@ -1,10 +1,11 @@
 
 from dagster import load_assets_from_package_module
-from src.prompt_engineering import database_preprocessing, basic_prompting
+from src.prompt_engineering import database_preprocessing, main_workflows, data_modeling
 
 # Group name for run pipelines
 PROMPT_ENGINEERING = "prompt_engineering"
 
 # Load all assets from different modules
 tables_preprocessing = load_assets_from_package_module(package_module=database_preprocessing, group_name=PROMPT_ENGINEERING)
-basic_prompting_wf = load_assets_from_package_module(package_module=basic_prompting, group_name=PROMPT_ENGINEERING)
+prompting_wf = load_assets_from_package_module(package_module=main_workflows, group_name=PROMPT_ENGINEERING)
+data_modeling_wf = load_assets_from_package_module(package_module=data_modeling, group_name=PROMPT_ENGINEERING)
