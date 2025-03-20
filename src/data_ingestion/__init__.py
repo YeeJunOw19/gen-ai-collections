d941e6872ccb41b91da1a28928fdade413ed6896
YeeJunOw19 <yeejunow@gmail.com>
2025-03-20 10:44:16 -0500

    Updated Dagster Workflow
    Updated Dagster workflow with new vector store functionality

--- src/data_ingestion/__init__.py
+++ src/data_ingestion/__init__.py
@@ -1,6 +1,6 @@
 
 from dagster import load_assets_from_package_module
-from src.data_ingestion import hugging_face, text_embedding
+from src.data_ingestion import hugging_face, text_embedding, python_embedding
 
 # Group name for run pipelines
 DATA_INGESTION = "data_ingestion"
@@ -9,3 +9,4 @@ VECTOR_EMBEDDING = "vector_embedding"
 # Load all assets from different modules
 raw_data_ingestion = load_assets_from_package_module(package_module=hugging_face, group_name=DATA_INGESTION)
 vector_embedding_wf = load_assets_from_package_module(package_module=text_embedding, group_name=VECTOR_EMBEDDING)
+python_embedding_wf = load_assets_from_package_module(package_module=python_embedding, group_name=DATA_INGESTION)
